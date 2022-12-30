64b0e1e4eecec23123f662fcf463e58e6612cb43
frappeerpnext <frapperpnext@gmail.com>
2022-12-30 16:39:25 +0700

    update filter in sale

--- sothearith/sothearith/doctype/sale/sale.js
+++ sothearith/sothearith/doctype/sale/sale.js
@@ -3,13 +3,17 @@
 
 frappe.ui.form.on("Sale", {
 	refresh(frm) {
-        
-		frm.set_query("customer_type", function() {
+        if(frappe.user.has_role("Sothearith HR")){ 
+			frm.set_query("customer_type", function() {
+				return {
+					filters: [["DocType","name","in",["Front Office","Purchasing","Human Resource","Sale and Marketing","Accounting","Finance","Law","Stock Control","IT and Maintenance"]]]
+				}
+			});
+		}else{
 			return {
-				
 				filters: [["DocType","name","in",["Customer","Customer Contractor","Customer Depot","Customer Project"]]]
 			}
-		});
+		}
 	},
 });
 
